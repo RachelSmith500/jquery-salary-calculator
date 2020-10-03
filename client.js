33d2861df8ea33e99addf7b2a97eb53af370a5aa
RachelSmith500 <rachelsmith9721@gmail.com>
2020-10-03 13:24:55 -0500

    Added submit button, value getters, and base table in html

--- client.js
+++ client.js
@@ -3,7 +3,17 @@ console.log('js')
 $(document).ready(onReady);
 
 function onReady(){
-
-
+//click listeners go here
 
 }    
+
+function submitEmployeeData(){
+    console.log('in submitEmployeeData Function');
+    //this is where the values will be grabbed
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let employeeId = $('#employeeId').val();
+    let title = $('#title').val();
+    let annualSalary = $('#annualSalary').val(); 
+    
+}
