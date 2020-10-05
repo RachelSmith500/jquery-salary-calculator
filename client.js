9588b93d51eb6c2f531166a6c8fe8fbd55aad6e9
RachelSmith500 <rachelsmith9721@gmail.com>
2020-10-04 23:27:06 -0500

    Notes Added

--- client.js
+++ client.js
@@ -1,13 +1,14 @@
 console.log('js')
-
+// global array 
 let employeeArray = [];
 
+//getting jquery ready 
 $(document).ready(onReady);
 
+//function for click listeners 
 function onReady(){
 //click listeners go here
 $('#submit').on('click', submitEmployeeData);
-console.log('submit form')
 }    
 
 function submitEmployeeData(){
@@ -18,7 +19,8 @@ function submitEmployeeData(){
     let employeeId = $('#employeeId').val();
     let title = $('#title').val();
     let annualSalary = $('#annualSalary').val(); 
-
+    
+    //storing data in an object that has been gathered from inputs
     let employeeObject = {
 
         firstNameKey: firstName,
@@ -27,33 +29,38 @@ function submitEmployeeData(){
         titleKey: title,
         annualSalaryKey: Number(annualSalary)
     }
-    
+    // alert set up that disables submit button if inputs are left blank 
     if((firstName === '') || (lastName === '') || (employeeId === '')||
     (title === '') || (annualSalary === 0)){
         alert('please fill all input values');
         return false;
     }
+    //pushing employee object into the employee array 
     employeeArray.push(employeeObject);
-
+    //clearing inputs 
     $('#firstName').val('');
     $('#lastName').val('');
     $('#employeeId').val('');
     $('#title').val('');
     $('#annualSalary').val('');
-
+    //calling buttonClick function 
     buttonClicked();
+    //click listener for delete 
     $('.delete').on('click', deleteFunction);
 }
-
+//delete function 
 function deleteFunction(){
     console.log('delete');
+    //remove this item that resides in the table data of the table row
     $(this).parent().parent().remove();
     // employeeArray.length = 0;
 }
-
+//function that appends the employee array data to the dom
 function buttonClicked(){
     let employeeToAdd = $("#employeeData");
+    //emptying the data before appending to the dom 
     employeeToAdd.empty();
+    //for loop appends the data from our array to the dom
     for(let i=0; i < employeeArray.length; i++){
         employeeToAdd.append
             (` 
@@ -67,9 +74,12 @@ function buttonClicked(){
                 </tr>
             `)
     }
+    //calling our salarySum function 
     annualSalarySum();
 }
-
+//this function adds the salaries of employees and divides the annual salary by 12 
+//it then appends that figure to the dom
+//if the sum is greater than 20k the monthly total turns red 
 function annualSalarySum(){
     let sum = 0;
     let salaryToAdd = $("#MonthlyCost");
